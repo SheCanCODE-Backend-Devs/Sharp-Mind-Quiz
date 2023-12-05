c26e41ad6d8afba1bf92848736596b170e378946
ingabire-byte <ingabirejoy25@gmail.com>
2023-12-05 16:33:04 +0200

    tuesdayWork

---
+++ Joy_Ingabire/question3.js
@@ -0,0 +1,10 @@
+// Reverse the provided string and return the reversed string.
+// For example, "hello" should become "olleh".
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(reverseString("Ingabire"));
+
+
